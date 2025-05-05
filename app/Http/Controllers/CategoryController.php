e217d53d36ea2bdc84d883b96571cda396e38549
nazwastpn <nazwasetiapin@gmail.com>
2025-05-05 13:31:04 +0700

    edit controller category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -39,7 +39,7 @@ class CategoryController extends Controller implements HasMiddleware
     }
 
     public function create()
-    {
+    { 
         return Inertia::render('Categories/Create');
     }
 
